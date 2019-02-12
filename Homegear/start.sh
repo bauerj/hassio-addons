47e5073e6b2e5acd3fabe55c862d8b58f22f2a6f
Johann Bauer <bauerj@bauerj.eu>
2019-02-12 23:59:43 +0100

    Update with upstream code

--- Homegear/start.sh
+++ Homegear/start.sh
@@ -1,16 +1,44 @@
 #/bin/bash
 
-# Inspired by https://github.com/Homegear/Homegear-Docker/blob/master/rpi-stable/start.sh
+_term() {
+	service homegear-influxdb stop
+	service homegear-management stop
+	service homegear-webssh stop
+	service homegear stop
+	exit $?
+}
 
-mkdir -p /config/homegear /share/homegear/lib /share/homegear/log
-chown homegear:homegear /config/homegear /share/homegear/lib /share/homegear/log
-rm -Rf /etc/homegear /var/lib/homegear /var/log/homegear
-ln -nfs /config/homegear     /etc/homegear
-ln -nfs /share/homegear/lib /var/lib/homegear
-ln -nfs /share/homegear/log /var/log/homegear
+trap _term SIGTERM
+
+USER=homegear
+
+USER_ID=$(id -u $USER)
+USER_GID=$(id -g $USER)
+
+USER_ID=${HOST_USER_ID:=$USER_ID}
+USER_GID=${HOST_USER_GID:=$USER_GID}
+
+if [ $USER_ID -eq 0 ]; then
+	sed -i "s/RUNASUSER=homegear/RUNASUSER=root/g" /etc/init.d/homegear
+	sed -i "s/RUNASGROUP=homegear/RUNASGROUP=root/g" /etc/init.d/homegear
+	sed -i "s/RUNASUSER=homegear/RUNASUSER=root/g" /etc/init.d/homegear-influxdb
+	sed -i "s/RUNASGROUP=homegear/RUNASGROUP=root/g" /etc/init.d/homegear-influxdb
+else
+	sed -i -e "s/^${USER}:\([^:]*\):[0-9]*:[0-9]*/${USER}:\1:${USER_ID}:${USER_GID}/" /etc/passwd
+	sed -i -e "s/^${USER}:\([^:]*\):[0-9]*/${USER}:\1:${USER_GID}/" /etc/group
+fi
+
+mkdir /config/homegear > /dev/null 2>&1
+ln -fs /config/homegear /etc/homegear
+
+mkdir -p /share/homegear/lib > /dev/null 2>&1
+mkdir -p /share/homegear/log > /dev/null 2>&1
+ln -fs /share/homegear/lib /var/lib/homegear
+ln -fs /share/homegear/log /var/log/homegear
 
 if ! [ "$(ls -A /etc/homegear)" ]; then
-	cp -R /etc/homegear.config/* /etc/homegear/
+	cp -a /etc/homegear.config/* /etc/homegear/
+        chmod -R a+r /etc/homegear/
 fi
 
 if ! [ "$(ls -A /var/lib/homegear)" ]; then
@@ -19,7 +47,11 @@ else
 	rm -Rf /var/lib/homegear/modules/*
 	rm -Rf /var/lib/homegear/flows/nodes/*
 	cp -a /var/lib/homegear.data/modules/* /var/lib/homegear/modules/
-	cp -a /var/lib/homegear.data/flows/nodes/* /var/lib/homegear/flows/nodes/
+	cp -a /var/lib/homegear.data/node-blue/nodes/* /var/lib/homegear/node-blue/nodes/
+fi
+
+if ! [ -f /var/log/homegear/homegear.log ]; then
+	touch /var/log/homegear/homegear.log
 fi
 
 if ! [ -f /etc/homegear/dh1024.pem ]; then
@@ -27,13 +59,25 @@ if ! [ -f /etc/homegear/dh1024.pem ]; then
 	openssl req -batch -new -key /etc/homegear/homegear.key -out /etc/homegear/homegear.csr
 	openssl x509 -req -in /etc/homegear/homegear.csr -signkey /etc/homegear/homegear.key -out /etc/homegear/homegear.crt
 	rm /etc/homegear/homegear.csr
-	chown homegear:homegear /etc/homegear/homegear.key
 	chmod 400 /etc/homegear/homegear.key
 	openssl dhparam -check -text -5 -out /etc/homegear/dh1024.pem 1024
-	chown homegear:homegear /etc/homegear/dh1024.pem
 	chmod 400 /etc/homegear/dh1024.pem
 fi
 
+chown -R root:root /etc/homegear
+find /etc/homegear -type d -exec chmod 755 {} \;
+chown -R homegear:homegear /var/log/homegear /var/lib/homegear
+find /var/log/homegear -type d -exec chmod 750 {} \;
+find /var/log/homegear -type f -exec chmod 640 {} \;
+find /var/lib/homegear -type d -exec chmod 750 {} \;
+find /var/lib/homegear -type f -exec chmod 640 {} \;
+
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
 service homegear start
+service homegear-management start
+service homegear-webssh start
 service homegear-influxdb start
-tail -f /var/log/homegear/homegear.log
+tail -f /var/log/homegear/homegear.log &
+child=$!
+wait "$child"
